df759a0143f7b24138bdd5836ea98a547caa2250
Juan Malagon <nicolas0915@hotmail.com>
2021-05-03 12:08:38 -0500

    First commit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('URL', 'http://localhost:82/endeavor_mentors/');
+define('URL_S3', 'https://mentors-day-assets.s3-us-west-2.amazonaws.com/assets/');
+define('HOST', 'localhost');
+define('DB', 'endeavor_mentors_live');
+define('USER', 'root');
+define('PASSWORD', '');
